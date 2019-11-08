9ca4e5cdf50acbed8d2afa846f1e66a2fcaa5683
jwlee <jwlee@f9af95d7-0ddc-463e-bccc-e8e81b23c893>
2019-11-08 13:46:28 +0000

    일정 조회 및 일정 추가 alert -> 모달로 수정

--- src/main/webapp/scripts/itsme/scheduleManage/ctrl_schedule_manage.js
+++ src/main/webapp/scripts/itsme/scheduleManage/ctrl_schedule_manage.js
@@ -11,8 +11,8 @@ app.controller("ScheduleManageCtrl", function($scope, $http) {
 				var param = {
 //						searchDate1 : date.slice(0,4) + "." + date.slice(5,7) + ".01",
 //						searchDate2 : date.slice(0,4) + "." + date.slice(5,7) + ".31"
-						searchDate1 : "",
-						searchDate2 : "2020.11.11"
+//						searchDate1 : "",
+//						searchDate2 : "2020.11.11"
 				}
 
 				req_http_rest_api.func.req_get_message($http, "/getSchedule", param, function(response){
@@ -46,25 +46,79 @@ app.controller("ScheduleManageCtrl", function($scope, $http) {
 					    	addEventButton: {
 					    		text: '일정 추가',
 					    		click: function() {
-					    			var dateStart = prompt('일정을 추가할 날짜를 입력하세요.(YYYY.MM.DD)');
-					    			var dateTitle = prompt('일정 내용을 입력하세요.');
-
-				    				if (dateStart.split(".").length == 3) { // valid?
-				    					$scope.scheduleManage.func.addSchedule(dateTitle, dateStart);
-				    					calendar.addEvent({
-				    						title: dateTitle,
-				    						start: dateStart.split(".").join("-"),
-				    						allDay: true
-				    					});
-				    				} else {
-				    					alert('Invalid date.');
-				    				}
+					    			var url = "templates/itsme/scheduleManage/model_content_addschedule.html";
+									// 다이얼로그를 생성합니다.
+									$('<div id="DialogDiv">').dialog({
+										// 커스텀 스타일을 줍니다.
+										dialogClass : 'custom-dialog-style',
+										// 모달 다이얼로그로 생성합니다.
+										modal : true,
+									    buttons: {
+									    	"추가": function() {
+									    		param = $scope.scheduleManage.func.getScheduleParam();
+												calendar.addEvent({
+													title: param.split("|")[0],
+													start: param.split("|")[1].split(".").join("-"),
+													allDay: true
+												});
+									    		$(this).dialog( "close" );
+									    	},
+									    	"삭제": function() {
+									    		$(this).dialog( "close" );
+									    	}
+									    },
+										// 다이얼로그 열기 콜백
+										open : function() {
+											// 모달 오버레이 설정
+											$(".ui-widget-overlay").css({
+												opacity : 0.5,
+												filter : "Alpha(Opacity=50)",
+												backgroundColor : "black"
+											});
+											// 내용을 불러 옵니다.
+											$(this).load(url);
+										},
+										// 닫기 콜백
+										close : function(e) {
+											$(this).empty();
+											$(this).dialog('destroy');
+										},
+										height : 400,
+										width : 700,
+										title : '일정 추가'
+									});
 					    		}
 					    	}
 					    },
 						events : list,
 						eventClick: function(arg) {
-							alert(arg.event.start + " : " + arg.event.title);
+							var url = "templates/itsme/scheduleManage/model_content_viewschedule.html";
+							// 다이얼로그를 생성합니다.
+							$('<div id="DialogDiv">').data('title', arg.event.title).data('start', arg.event.start).dialog({
+								// 커스텀 스타일을 줍니다.
+								dialogClass : 'custom-dialog-style',
+								// 모달 다이얼로그로 생성합니다.
+								modal : true,
+								// 다이얼로그 열기 콜백
+								open : function() {
+									// 모달 오버레이 설정
+									$(".ui-widget-overlay").css({
+										opacity : 0.5,
+										filter : "Alpha(Opacity=50)",
+										backgroundColor : "black"
+									});
+									// 내용을 불러 옵니다.
+									$(this).load(url);
+								},
+								// 닫기 콜백
+								close : function(e) {
+									$(this).empty();
+									$(this).dialog('destroy');
+								},
+								height : 400,
+								width : 700,
+								title : '일정 조회'
+							});
 					    }
 				});
 
@@ -75,6 +129,17 @@ app.controller("ScheduleManageCtrl", function($scope, $http) {
 						
 					});
 				}
+			},
+			getScheduleParam : function(){
+	        	var dateTitle = $("#title").val();
+    			var dateStart = $("#date").val();
+
+				if (dateStart.split(".").length == 3) { // valid?
+					$scope.scheduleManage.func.addSchedule(dateTitle, dateStart);
+				} else {
+					alert('Invalid date.');
+				}
+				return dateTitle + "|" + dateStart;
 			},
 			addSchedule : function(title, start) {
 				var param = {
